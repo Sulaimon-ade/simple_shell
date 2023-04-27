cd553796d632c64ed02752fa1776b3edbf67e1e2
sulaimon-ade <okunoyesouleiman@gmail.com>
2023-04-27 00:39:54 -0700

    final commit

---
+++ kcshell/betty.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *betty - print a simple number from
+ * 1 - 20
+ * Return: Always 0
+ */
+int betty(void)
+{
+	int i;
+	int betty;
+
+	for (i = 1; i <= 20; i++)
+		return (i);
+	betty = i + 2;
+	return (betty);
+}
