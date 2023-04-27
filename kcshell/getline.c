f75a3dcc584747359a24beefc6c946379bce1e1c
sulaimon-ade <okunoyesouleiman@gmail.com>
2023-04-26 17:43:20 -0700

    all files

--- kcshell/getline.c
+++ kcshell/getline.c
@@ -165,6 +165,6 @@ int _getline(info_t *info, char **ptr, size_t *length)
 void sigintHandler(__attribute__((unused))int sig_num)
 {
 	_puts("\n");
-	_puts("$ ");
+	_puts("KingNco$ ");
 	_putchar(BUF_FLUSH);
 }
